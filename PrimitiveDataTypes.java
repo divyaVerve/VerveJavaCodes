f7b3693a0166ee5112fea72ccd94d5991dbe1be5
divyaVerve <94595801+divyaVerve@users.noreply.github.com>
2021-11-22 15:06:03 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,43 @@
+package javaBasics;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive data types
+		// boolean
+		 boolean boolean1=true;
+	     boolean boolean2=false;
+	     System.out.println("Boolean Value="+boolean1);
+	     System.out.println("Boolean Value="+boolean2);
+	     
+	     // Character
+	     char charGender='F';
+	     System.out.println("Gender="+charGender);
+	     
+	     //Integer
+        int intAge = 25;
+        System.out.println("Age="+intAge);
+        
+        //byte
+        byte byteRollNo=10;
+        System.out.println("RollNumber="+byteRollNo);
+        
+        //short
+        short shortRegNo=23980;
+        System.out.println("Registration number="+shortRegNo);
+        
+        //long
+        long longUniNo=128000L;
+        System.out.println("University number="+longUniNo);
+        
+        //double
+        double doubleWeight=45.6;
+        System.out.println("Weight="+doubleWeight);
+         
+        //float
+        float floatNumber= 230.5f;
+        System.out.println("Number="+floatNumber);
+	}
+
+}
