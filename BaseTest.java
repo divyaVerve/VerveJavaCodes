5937e23ae999a5314ec64735b09bf02869907f25
divyaVerve <94595801+divyaVerve@users.noreply.github.com>
2021-11-30 18:37:49 +0530

    Add files via upload

---
+++ BaseTest.java
@@ -0,0 +1,14 @@
+package com.generic;
+
+public class BaseTest {
+	public void initialiseMeEnvironment()
+	{
+		System.out.println("Open URL:");
+	}
+	
+	public void tearDown()
+	{
+		System.out.println("Close the browser");
+	}
+
+}
